74477cc144f86e3793c8423be902c7b4cf03daaf
saibrahma450 <aibrahma.maroju450@gmail.com>
2021-06-17 12:51:12 +0000

    java

---
+++ demo.java
@@ -0,0 +1,14 @@
+class Demo
+{
+    public static void main (....)
+{
+    s.o.print("Good Day");
+
+    s.o.print("Good Day");
+    s.o.print("Good Day");
+    s.o.print("Good Day");
+    s.o.print("Good Day");
+    s.o.print("Good Day");
+    s.o.print("Good Day");
+}
+}
